7c2f274224414e606c91d0c9b55d18fd78db486f
David Ryder <67267592+david-ryder@users.noreply.github.com>
2023-07-14 02:50:46 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask, request, jsonify
+import psycopg2
+
+app = Flask('__name__')
+
+# Define configuration for the database
+app.config['DATABASE'] = {
+    'host': 'localhost',
+    'port': '5432',
+    'database': 'million',
+    'user': 'postgres',
+    'password': 'postgres'
+}
+
+# Establish connection with database
+def get_db():
+    conn = psycopg2.connect(
+        host=app.config['DATABASE']['host'],
+        port=app.config['DATABASE']['port'],
+        dbname=app.config['DATABASE']['database'],
+        user=app.config['DATABASE']['user'],
+        password=app.config['DATABASE']['password']
+    )
+    return conn
+
+# Post new message to database
+@app.route('/messages', methods=['POST'])
+def create_message():
+    message = request.json.get('message')
+
+    conn = get_db()
+    cur = conn.cursor()
+
+    cur.execute('INSERT INTO messages (message) VALUES (%s)', (message,))
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    return "Message inserted successfully!"
+
+# Get all messages from database
+@app.route('/messages', methods=['GET'])
+def read_messages():
+    conn = get_db()
+    cur = conn.cursor()
+
+    cur.execute('SELECT * FROM messages')
+    rows = cur.fetchall()
+
+    messages = []
+    for row in rows:
+        message = {
+            'id': row[0],
+            'message': row[1]
+        }
+        messages.append(message)
+
+    cur.close()
+    conn.close()
+
+    return jsonify(messages)
+
+# Run the API
+if __name__ == '__main__':
+    app.run()
---
+++ setup.md
@@ -0,0 +1,9 @@
+Overall idea:
+create a site hosted on github pages that basically lets me see/add ideas for project ideas
+
+# Very simple UI
+- Title
+- List of ideas:
+- Textbox to add more
+- When enter key pressed, or submit button pressed, idea gets added to the list
+
