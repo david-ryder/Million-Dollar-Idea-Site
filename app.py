6c6b8ef3393216156137425e624671af9fbc9817
David Winfield <67267592+david-ryder@users.noreply.github.com>
2023-07-17 02:27:38 -0400

    containerized with docker

--- app.py
+++ app.py
@@ -3,32 +3,49 @@ import psycopg2
 
 app = Flask('__name__')
 
-# Define configuration for the database
-app.config['DATABASE'] = {
-    'host': 'localhost',
-    'port': '5432',
-    'database': 'million',
-    'user': 'postgres',
-    'password': 'postgres'
-}
-
-# Establish connection with database
-def get_db():
+def connect_to_database():
     conn = psycopg2.connect(
-        host=app.config['DATABASE']['host'],
-        port=app.config['DATABASE']['port'],
-        dbname=app.config['DATABASE']['database'],
-        user=app.config['DATABASE']['user'],
-        password=app.config['DATABASE']['password']
+        host="db", 
+        port=5432,
+        user="postgres",
+        password="postgres",
+        database="million"
     )
     return conn
+
+def create_schema():
+    conn = None
+    try:
+        conn = connect_to_database()
+        cur = conn.cursor()
+
+        # Create the 'messages' table
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS messages (
+                id SERIAL PRIMARY KEY,
+                message TEXT
+            );
+        """)
+
+        conn.commit()
+        cur.close()
+        print("Schema created successfully")
+    except (Exception, psycopg2.DatabaseError) as error:
+        print("Error creating schema:", error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+# Call create_schema() when the Flask API connects to the database
+with app.app_context():
+    create_schema()
 
 # Post new message to database
 @app.route('/messages', methods=['POST'])
 def create_message():
     message = request.json.get('message')
 
-    conn = get_db()
+    conn = connect_to_database()
     cur = conn.cursor()
 
     try:
@@ -48,7 +65,7 @@ def create_message():
 # Get all messages from database
 @app.route('/messages', methods=['GET'])
 def read_messages():
-    conn = get_db()
+    conn = connect_to_database()
     cur = conn.cursor()
 
     cur.execute('SELECT * FROM messages')
